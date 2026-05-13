49731800c351380922118ff0789ebe0a6253803a
sofianelamine772-design <mwcrea.agency@gmail.com>
2026-05-13 13:00:07 +0200

    d

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -11,7 +11,7 @@ const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
 export async function POST(req: Request) {
   const body = await req.text();
-  const signature = headers().get('stripe-signature') as string;
+  const signature = (await headers()).get('stripe-signature') as string;
 
   let event: Stripe.Event;
 
